cee02f0f403963abe70a7e56a4ac593258083e06
rizkialfiannoor011 <rizkialfin110@gmail.com>
2020-04-26 08:55:48 -0700

    kondisi Statemen 2 jalan

--- src/Percabangan/IfElse.java
+++ src/Percabangan/IfElse.java
@@ -2,8 +2,8 @@ package Percabangan;
 
 public class IfElse {
     public static void main (String[] args){
-        double totalBelanja = 53000;
-        double uangDiDompet = 25000;
+        double totalBelanja = 25000;
+        double uangDiDompet = 50000;
 
         if (uangDiDompet<totalBelanja){
             System.out.println("Uang anda kurang, Kurangi belanjaan anda");
