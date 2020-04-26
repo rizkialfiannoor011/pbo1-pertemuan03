a193ea03220d97390410ce72653d9356f3b44598
rizkialfiannoor011 <rizkialfin110@gmail.com>
2020-04-26 09:28:34 -0700

    Contoh Switch Menggunakan char

--- src/Percabangan/Switch.java
+++ src/Percabangan/Switch.java
@@ -1,26 +1,29 @@
 package Percabangan;
 
 public class Switch {
-    public static void main ( String[] args){
-        int JalanJalan = 3;
-        switch (JalanJalan){
-            case 1:
-                System.out.println("makan Bakso");
+    public static void main(String[] args) {
+        char nilai = 'D';
+        switch (nilai) {
+
+            case 'A':
+                System.out.println("Mhs     : \"Terimakasih Pak\"");
+                System.out.println("Dosen   : \"Selamat yaa\"");
                 break;
-            case 2:
-                System.out.println("makan Sate");
+            case 'B':
+                System.out.println("Mhs     : \"Izin bertanya pak\"");
+                System.out.println("Dosen   : \"yaa silahkan\"");
                 break;
-            case 3:
-                System.out.println("makan Ayam Goreng ");
-                break;
-            case 4:
-                System.out.println("minum Es Jeruk");
-                break;
-            case 5:
-                System.out.println("minum Teh Es");
+            case 'C':
+                System.out.println("Mhs     : \"Ulun turun pul pak, tugas ngumpul jua kenapa dapat c?\"");
+                System.out.println("Dosen   : \"Ujian Menjawab bujur lah?\"");
+                System.out.println("Mhs     : \"melihat di kawan pang pa ai smalam\"");
                 break;
             default:
-                System.out.println("Sudah Makan");
+                System.out.println("Mhs     : \"Ulun turun pul pak, tugas ngumpul jua kenapa dapat c?\"");
+                System.out.println("Dosen   : \"Ujian Menjawab bujur lah?\"");
+                System.out.println("Mhs     : \"melihat di kawan pang pa ai smalam\"");
+                System.out.println("Dosen   : \"Yuk sini kita llihati Kartas Ujian km\"");
+                System.out.println("Mhs     : \"ehee... ulun di telponi mama pak disuruh bulik.. Assalamualaikum .....\"");
                 break;
         }
     }
