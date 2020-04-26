ae63c43d506126e520ee6a0e4eadfe7cece85c69
rizkialfiannoor011 <rizkialfin110@gmail.com>
2020-04-26 08:11:52 -0700

    Operator Binary Assignment

--- src/Operator/OperatorBinary.java
+++ src/Operator/OperatorBinary.java
@@ -1,12 +1,13 @@
 package Operator;
 
 public class OperatorBinary {
-    
-    //operator (=) sebagai assignmenr/penugasan
+
+    //operator (=) sebagai assignmenr/penugasann
     //menempatkan nilai kedalam variable
     //baris 5-7 adalah comment/komentar yang tidak dijalankan oleh program
 
     String nama = " Rizki Alfiannoor";
     int jumlahSks = 24;
     double indeksPrestasiKomulatif = 3.5;
+
 }
