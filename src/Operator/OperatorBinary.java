87bb012e8823ce1cef04ff944ceb66052123417c
rizkialfiannoor011 <rizkialfin110@gmail.com>
2020-04-26 08:17:02 -0700

    Oerator Binary Aritmatika

--- src/Operator/OperatorBinary.java
+++ src/Operator/OperatorBinary.java
@@ -10,4 +10,13 @@ public class OperatorBinary {
     int jumlahSks = 24;
     double indeksPrestasiKomulatif = 3.5;
 
+    public static void main (String[] args){
+        System.out.println(9+3);
+        System.out.println(9-3);
+        System.out.println(9*3);
+        System.out.println(9/3);
+        System.out.println(9%3);
+        System.out.println(9%2);
+
+    }
 }
