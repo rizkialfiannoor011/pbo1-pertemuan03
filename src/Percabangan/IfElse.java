57b221c939592b871477b78647e57356d09b88b0
rizkialfiannoor011 <rizkialfin110@gmail.com>
2020-04-26 09:02:52 -0700

    kondisi 1 salah dan posisi kondisi 2 benar sehinggan yang di jalankan adalah statemen 2

--- src/Percabangan/IfElse.java
+++ src/Percabangan/IfElse.java
@@ -3,12 +3,14 @@ package Percabangan;
 public class IfElse {
     public static void main (String[] args){
         double totalBelanja = 25000;
-        double uangDiDompet = 50000;
+        double uangDiDompet = 25000;
 
         if (uangDiDompet<totalBelanja){
             System.out.println("Uang anda kurang, Kurangi belanjaan anda");
-        }else {
+        }else if (uangDiDompet>totalBelanja){
             System.out.println("Uang anda cukup, selamat menikmati");
+        }else {
+            System.out.println("Anda Membayar dengan uang pass");
         }
     }
 }
