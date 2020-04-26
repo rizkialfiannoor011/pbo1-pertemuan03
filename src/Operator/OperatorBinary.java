443cf42f7cbdaa757097d92f0cc0f21f7430e96c
rizkialfiannoor011 <rizkialfin110@gmail.com>
2020-04-26 08:10:39 -0700

    toUpperCase untuk Mengubah hurup menjadi kapital semua toLowerCase untuk Mengubah hurup menjadi kecil semua substring(5) untuk menulis setelah 5 indeks pertama substring(0,4) untuk menulis 4 hurup pertama

---
+++ src/Operator/OperatorBinary.java
@@ -0,0 +1,12 @@
+package Operator;
+
+public class OperatorBinary {
+    
+    //operator (=) sebagai assignmenr/penugasan
+    //menempatkan nilai kedalam variable
+    //baris 5-7 adalah comment/komentar yang tidak dijalankan oleh program
+
+    String nama = " Rizki Alfiannoor";
+    int jumlahSks = 24;
+    double indeksPrestasiKomulatif = 3.5;
+}
